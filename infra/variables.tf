953eccd4944826ffb12179333e657721b9725ac4
Alvin <alvinjanton575@gmail.com>
2026-04-03 19:39:48 -0400

    Removing error in terraform code

--- infra/variables.tf
+++ infra/variables.tf
@@ -64,7 +64,7 @@ variable "cognito_domain_prefix" {
 variable "kms_key_id" {
   description = "ID or alias of the existing KMS key (e.g. alias/iamdash-prod-logs)"
   type        = string
-  default     = "arn:aws:kms:us-east-1:562559071105:key/9fa1e2a4-3ed2-4c6d-a2b4-4542904f47cc"
+  default     = ""
   validation {
     condition     = length(var.kms_key_id) > 0
     error_message = "kms_key_id must be set via TF_VAR_kms_key_id, terraform.tfvars, or environment-specific configuration."
